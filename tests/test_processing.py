a2c80c931131e3aa244b992731365c0d3f737433
John Truckenbrodt <john.truckenbrodt@dlr.de>
2025-12-10 19:29:10 +0100

    [tests] replaced SAR_IMP_1P product (to be able to use REAPER OSVs)

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -9,7 +9,7 @@ import asard.processor as process
         ('ASAR', 'APS', '2003-07-15', '31UFT'),
         ('ASAR', 'IMP', '2011-08-20', '32UPB'),
         ('ASAR', 'IMS', '2008-09-09', '19JFN'),
-        ('ERS1', 'IMP', '1996-08-08', '33VVD'),
+        ('ERS1', 'IMP', '1996-03-01', '33VWE'),
         ('ERS1', 'IMS', '1994-06-24', '43RGL'),
         ('ERS2', 'IMP', '1997-04-01', '11TME'),
         ('ERS2', 'IMS', '2002-10-10', '47QQG')
